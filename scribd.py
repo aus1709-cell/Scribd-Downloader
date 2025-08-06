fac41a5816f5e3c117c00ebe392a5fc39833cc37
aus1709-cell <aus1709@gmail.com>
2025-08-06 11:14:12 +0700

    Update scribd.py

--- scribd.py
+++ scribd.py
@@ -13,7 +13,7 @@ if len(sys.argv) ==1:
 	print 'Usage: sudo python scribd.py <link of scribd document>'
 	print
 	print 'For selectable PDFs:'
-	print '- example: sudo python scribd.py https://www.scribd.com/document/55949937/33-Strategies-of-War'
+	print '- example: sudo python scribd.py https://www.scribd.com/document/793416003/%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%84%E0%B8%B3%E0%B8%99%E0%B8%A7%E0%B8%93%E0%B9%80%E0%B8%81%E0%B8%B5-%E0%B8%A2%E0%B8%A7%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B8%84-%E0%B8%B2%E0%B8%84%E0%B8%87%E0%B8%97%E0%B8%B5-%E0%B8%AA%E0%B8%A1%E0%B8%94%E0%B8%B8%E0%B8%A5'
 	print
 	print 'For PDFs containing Images; use the -p option:'
 	print '- example: sudo python scribd.py http://scribd.com/doc/17142797/Case-in-Point -p'
